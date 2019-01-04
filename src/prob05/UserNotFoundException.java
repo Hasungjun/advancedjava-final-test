72aee4cc807e57af7ef1363f1fbadbb5aa3634e7
bit <bit@DESKTOP-3HOLO2D>
2019-01-04 17:00:19 +0900

    end

--- src/prob05/UserNotFoundException.java
+++ src/prob05/UserNotFoundException.java
@@ -4,6 +4,9 @@ public class UserNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
+	public UserNotFoundException() {
+		super("사용자를 찾을 수 없습니다.");
+	}
 
 
 }
